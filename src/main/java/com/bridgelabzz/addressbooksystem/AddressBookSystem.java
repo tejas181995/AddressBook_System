5a16058153485c758072858a060c0cc1f984890a
tejas181995 <tejasdev95@gmail.com>
2021-03-19 22:39:35 +0530

    new file created

---
+++ src/main/java/com/bridgelabzz/addressbooksystem/AddressBookSystem.java
@@ -0,0 +1,7 @@
+package com.bridgelabzz.addressbooksystem;
+
+public class AddressBookSystem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address book system");
+    }
+}
