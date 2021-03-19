b3e02f887af6a093b8063e205b017f50a7bd376f
tejas181995 <tejasdev95@gmail.com>
2021-03-19 22:48:33 +0530

    Added contact in addressbook

--- src/main/java/com/bridgelabzz/addressbooksystem/AddressBookSystem.java
+++ src/main/java/com/bridgelabzz/addressbooksystem/AddressBookSystem.java
@@ -1,7 +1,17 @@
 package com.bridgelabzz.addressbooksystem;
 
+import java.util.Scanner;
+
 public class AddressBookSystem {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Address book system");
+        String[] values = new String[8];
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < values.length; i++) {
+            System.out.println("enter" + Contact.fields[i] + ": ");
+            values[i] = sc.nextLine();
+        }
+        Contact contact = new Contact(values);
+        contact.printContact();
     }
 }
---
+++ src/main/java/com/bridgelabzz/addressbooksystem/Contact.java
@@ -0,0 +1,15 @@
+package com.bridgelabzz.addressbooksystem;
+
+public class Contact {
+    public static final String[] fields = new String[]{"firstName", "lastName", "address", "city", "state", "zipCode", "phoneNumber", "email" } ;
+    private String[] values;
+    public Contact(String[] values){
+        this.values = values;
+
+    }
+    public void printContact(){
+        for(int i=0; i<fields.length; i++){
+            System.out.println(fields[i] + ":" + values[i]);
+        }
+    }
+}
