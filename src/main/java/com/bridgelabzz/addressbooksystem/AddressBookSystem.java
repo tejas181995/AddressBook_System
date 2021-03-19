0c11329fd58e28e1a5281148dd6733a06b3f50bb
tejas181995 <tejasdev95@gmail.com>
2021-03-19 22:57:10 +0530

    Added method for new contact

--- src/main/java/com/bridgelabzz/addressbooksystem/AddressBookSystem.java
+++ src/main/java/com/bridgelabzz/addressbooksystem/AddressBookSystem.java
@@ -1,17 +1,30 @@
 package com.bridgelabzz.addressbooksystem;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBookSystem {
 
+    ArrayList<Contact> contactBook = new ArrayList<Contact>();
+
+    public int addContacts(Contact contact){
+        contactBook.add(contact);
+        contact.printContact();
+        return contactBook.size();
+
+    }
     public static void main(String[] args) {
+        int totalContacts;
+        AddressBookSystem addressBook = new AddressBookSystem();
         String[] values = new String[8];
         Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < values.length; i++) {
+        for(int i=0; i<values.length; i++){
             System.out.println("enter" + Contact.fields[i] + ": ");
             values[i] = sc.nextLine();
         }
         Contact contact = new Contact(values);
         contact.printContact();
+        totalContacts = addressBook.addContacts(contact);
+        System.out.println("Total contacts is Address Book: " + totalContacts);
     }
 }
